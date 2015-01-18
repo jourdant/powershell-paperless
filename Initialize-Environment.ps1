11c73a6c36dae9e06523368eff1b958c3cca6963
Jourdan Templeton <j@en.vc>
2015-01-18 17:52:41 +1300

    Updated Initialise-Environment.ps1 to include iTextSharp libraries. Still using nuget packages to get latest binaries.

--- Initialize-Environment.ps1
+++ Initialize-Environment.ps1
@@ -2,15 +2,17 @@
 # Title:     Initialize-Environment.ps1
 # Author:    Jourdan Templeton
 # Email:     hello@jourdant.me
-# Modified:  04/01/2015 05:44PM NZDT
+# Modified:  18/01/2015 05:52PM NZDT
 #
 
 #properties
 #================================================================
 $tesseract_url = "https://nuget.org/api/v2/package/Tesseract"
 $tessdata_url = "https://nuget.org/api/v2/package/tesseract-ocr"
+$itextsharp_url = "https://nuget.org/api/v2/package/iTextSharp"
 $tesseract_zip_name = "tesseract.zip"
 $tessdata_zip_name = "tessdata.zip"
+$itextsharp_zip_name = "itextsharp.zip"
 
 $input_dir_name = "Input"
 $output_dir_name = "Output"
@@ -36,7 +38,7 @@ If ((Test-Path $tesseract_zip_name) -eq $True)
 	$zip = [IO.Compression.ZipFile]::OpenRead($tesseract_zip_name)
 	
 	#extract tesseract libraries
-	$zip.Entries | Where FullName -match "(x86|x64)|net451/tesseract\.(?:dll|xml)" | % {
+	$zip.Entries | Where FullName -match "(x86|x64)|net451/tesseract\.dll" | % {
 		$dir = (Get-Item $lib_dir_name).FullName
 		If ($_.FullName.Contains("content")) { $dir +=  "\" + $matches[0] }
 		If ((Test-Path $dir) -eq $False) { mkdir $dir | Out-Null }
@@ -46,6 +48,7 @@ If ((Test-Path $tesseract_zip_name) -eq $True)
 	}
 
 	$zip.Dispose()
+	Remove-Item $tesseract_zip_name -Force
 }
 
 #download and extract tesseract data files
@@ -68,7 +71,26 @@ If ((Test-Path $tessdata_zip_name) -eq $True)
 	}
 
 	$zip.Dispose()
+	Remove-Item $tessdata_zip_name -Force
 }
 
-#remove temp zip files
-Remove-Item tess*.zip -Force
+#download and extract tesseract data files
+If ((Test-Path $itextsharp_zip_name) -eq $False) { 
+	Write-Host "Downloading:" $itextsharp_url "  To:" $itextsharp_zip_name
+	Invoke-WebRequest -Uri $itextsharp_url -OutFile  $itextsharp_zip_name
+}
+
+If ((Test-Path $itextsharp_zip_name) -eq $True)
+{
+	$zip = [IO.Compression.ZipFile]::OpenRead($itextsharp_zip_name)
+	
+	#extract itextsharp libraries
+	$zip.Entries | Where FullName -match "itextsharp.dll" | % {
+		$dir = (Get-Item $lib_dir_name).FullName
+		$file = $dir + "\" + $_.Name
+		[IO.Compression.ZipFileExtensions]::ExtractToFile($_, $file, $true) 
+	}
+
+	$zip.Dispose()
+	Remove-Item $itextsharp_zip_name -Force
+}
